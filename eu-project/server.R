1da784cdbbade6448a49ef910a7d78bdef1173f7
Jennifer Tao <1365673620@qq.com>
2018-12-01 18:59:01 -0800

    add app frame

--- eu-project/server.R
+++ eu-project/server.R
@@ -8,18 +8,17 @@
 #
 
 library(shiny)
+library(R.utils)
+library(shinythemes)
+
+projects <- data.table::fread("data/projects.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   
     
   })
   
--- eu-project/ui.R
+++ eu-project/ui.R
@@ -8,26 +8,27 @@
 #
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+  theme = shinytheme("cosmo"),
+  navbarPage("Navbar!",
+             tabPanel("About"
+                      
+             ),
+             tabPanel("Funding",
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                        ),
+                        mainPanel(
+                          
+                        )
+                      )
+             ),
+             tabPanel("Energy projects"
+                      
+             )
   )
 ))
